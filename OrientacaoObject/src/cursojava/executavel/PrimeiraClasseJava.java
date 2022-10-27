3697c3bf6c6009f13f4d30f463698e39c2819b82
WilhamsGomes <wilhamsgoomes@gmail.com>
2022-10-27 20:50:20 -0300

    commit envio

--- OrientacaoObject/src/cursojava/executavel/PrimeiraClasseJava.java
+++ OrientacaoObject/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -34,7 +34,7 @@ public class PrimeiraClasseJava {
 				
 				String nome = JOptionPane.showInputDialog("Qual o nome do aluno "+qtd+"?");
 				System.out.println("TESTE");
-				System.out.println("Passei daquiaaaaaaaaaaa");
+				System.out.println("Passei daquiaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 				/*String idade = JOptionPane.showInputDialog("Qual a idade do aluno?");
 				String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento do aluno?");
 				String registroGeral = JOptionPane.showInputDialog("Qual o RG do aluno?");
